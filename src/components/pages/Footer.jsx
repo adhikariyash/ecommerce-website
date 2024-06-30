9674b0a238868b0ec41c80c5bca203258fa386ed
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-06-30 19:21:42 +0545

    second commit part one

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
    <div>
-    <div className="main p-4 ">
+    < div className="main p-4 ">
         <div>
          <h2>Get to know us</h2>
         </div>
-        <div>
-      
-
-        </div>
-        <div>
-
-        </div>
-    </div>
-   </div>
+     
+  </div>
+  </div>
   )
 }
 
